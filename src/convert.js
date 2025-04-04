4d97c53126fc2903cc65c4e1dca5eea631e66dfe
cejidb <cejidb@mail.ru>
2025-04-04 16:21:31 +0300

    feat: add convert.js

--- src/convert.js
+++ src/convert.js
@@ -1,20 +1,8 @@
-const convert = (arr) => {
-  const result = arr.reduce((acc, element) => {
-    if (Array.isArray(element)) {
-      const [key, value] = element;
-
-      if (!Array.isArray(key) && !Array.isArray(value)) {
-        const newObj = {};
-        newObj[key] = value;
-
-        return { ...acc, ...newObj };
-      }
-
-      return convert(element);
-    }
+const convert = (tree) =>
+  tree.reduce((acc, node) => {
+    const [key, value] = node;
+    const newValue = Array.isArray(value) ? convert(value) : value;
+    return { ...acc, [key]: newValue };
   }, {});
-
-  return result;
-};
 
 export default convert;
